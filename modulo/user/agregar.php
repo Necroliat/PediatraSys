1864d717a7020248e73c6bfa7432b1352706573e
Joel Rosario <joelenrosario@gmail.com>
2024-05-26 16:38:43 -0400

    uuuuu

--- modulo/user/agregar.php
+++ modulo/user/agregar.php
@@ -31,6 +31,19 @@ $userid = $newId;
     <link rel="icon" type="image/x-icon" href="IMAGENES/hospital2.ico">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/estilo-paciente.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+	<meta charset="UTF-8">
+	<!-- <link rel="stylesheet" type="text/css" href="css/estilo-paciente.css"> -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<script src="https://kit.fontawesome.com/726ca5cfb3.js" crossorigin="anonymous"></script>
+	<meta charset="UTF-8">
+	<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+	<!-- <link rel="stylesheet" type="text/css" href="css/estilo-paciente.css"> -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
 
     <style>
